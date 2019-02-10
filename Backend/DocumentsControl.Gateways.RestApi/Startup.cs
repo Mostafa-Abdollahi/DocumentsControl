3186e0bbd6ab2f71e13d337b6136a5f5537404dc
Hafez <hafez.gh.moahammadi@gmail.com>
2019-02-10 10:15:15 +0330

    Change Startup

--- Backend/DocumentsControl.Gateways.RestApi/Startup.cs
+++ Backend/DocumentsControl.Gateways.RestApi/Startup.cs
@@ -27,7 +27,7 @@ namespace DocumentsControl.Gateways.RestApi
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("kose ammat!");
             });
         }
     }
